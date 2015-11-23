7a4cfe1cb256b1abedc0a03dffa139dc5ae08464
mnc2006 <mnc2006@hotmail.com>
2015-11-23 17:01:09 -0500

    initial app build

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,29 @@
+class TodoItemsController < ApplicationController
+
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    @todo_item = todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo list item was deleted."
+    else
+      flash[:error] = "Todo list item could not be deleted."
+    end
+    redirect_to @todo_list
+
+  private
+
+  def set_todo_list
+    @todo_list = Todolist.find(params[:todo_list_id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
+end
+
+
+end
