ede2c7ae6710321d1bacb3a681de4af5394d4255
mnc2006 <mnc2006@hotmail.com>
2015-11-29 16:14:31 -0500

    rolled back welcome migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  self.responder = ApplicationResponder
+
   helper_method :current_user_session, :current_user
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,14 @@ Rails.application.routes.draw do
     member do
       patch :complete
   end
-end
 
-resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create]
 
-resources :user_sessions, only: [:create, :destroy]
-delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
-get '/sign_in', to: 'user_sessions#new', as: :sign_in 
+  resources :user_sessions, only: [:create, :destroy]
+  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+  get '/sign_in', to: 'user_sessions#new', as: :sign_in
 
-
+  end
 end
 
 
